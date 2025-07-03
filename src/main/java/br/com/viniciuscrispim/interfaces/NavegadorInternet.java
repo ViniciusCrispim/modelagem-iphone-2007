1401ada99d326bc2691c4ef1776f8c9f274be073
Vinícius Crispim <viniciuswoliveira@outlook.com>
2025-07-03 19:07:23 -0300

    Implementação interface NavegadorInternet

---
+++ src/main/java/br/com/viniciuscrispim/interfaces/NavegadorInternet.java
@@ -0,0 +1,9 @@
+package br.com.viniciuscrispim.interfaces;
+
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+
+    void adicionarNovaAba();
+
+    void atualizarPagina();
+}
